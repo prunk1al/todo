ca0bdf5777a0f3e8c24d13bceaf7a56b8215559e
prunk1al <prunk1al@gmail.com>
2015-12-15 06:30:09 +0100

    su7

--- main.py
+++ main.py
@@ -1,65 +1,41 @@
 import tornado.ioloop
 import tornado.web
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import json
 
+client= MongoClient('localhost', 27017)
+db=client['todo']
+collection=db['tasks']
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-class ocupacion(tornado.web.RequestHandler):
-    def get(self):
-        self.render('ocupacion.1.html')
-
-
 class tasksJson(tornado.web.RequestHandler):
     def get(self):
         data=[]
-        client= MongoClient('localhost', 27017)
-        db=client['todo']
-        collection=db['tasks']
-        """task = {"name": "prueba",
-                "descripcion": "description",
-                "projecto": "prueba",
-                "relevancia": 1}
-        collection.insert_one(task)"""
+       
         datas=collection.find()
         for d in datas:
-            d.pop('_id')
+            d['_id']=str(d['_id'])
             data.append(d)
         self.write(json.dumps(data))
 
-class altas(tornado.web.RequestHandler):
-    def get(self):
-        self.render('altas.html')
+class Update(tornado.web.RequestHandler):
+    def post(self):
+        data=json.loads(self.request.body)
+        filter={'_id':ObjectId(data['_id'])}
+        del data['_id']
+        task=collection.update_one(filter,{'$set':data} )
+        print task
 
-
-class altasJson(tornado.web.RequestHandler):
-    def get(self):
-        data=[]
-        client= MongoClient('192.168.0.155', 27017)
-        db=client['statistics']
-        altas=db['altas']
-        datas=altas.find({'$and':[
-                                {'Fecha_Alta':{'$gte':'2015-08-17'}},
-                                {'Fecha_Alta':{'$lte':'2015-08-19'}}
-                                ]
-                          })
-        for d in datas:
-            d.pop('_id')
-            data.append(d)
-
-
-        self.write(json.dumps(data))
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/altas.html", altas),
-        (r"/altas.json", altasJson),
-        (r"/ocupacion.1.html", ocupacion),
         (r"/tasks.json", tasksJson),
+        (r"/update", Update),
          (r'/js/(.*)', tornado.web.StaticFileHandler, {'path': './js'})
     ], debug=True)
 
